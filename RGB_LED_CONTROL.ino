0283a105edcb32012fc20f732b44003cc072b13e
toluwalasewashere <haastrup.toluwalase@gmail.com>
2026-03-11 01:03:57 -0400

    Modified RGB_LED_CONTROL.ino - Needs to be tested on board

--- RGB_LED_CONTROL.ino
+++ RGB_LED_CONTROL.ino
@@ -4,22 +4,23 @@
 
 const int dataPin = 12; /*Arduino pin 12 will be connected to SR pin 14 (Serial input pin)*/
 const int enablePin = 11; /*Arduino pin 11 will be connected to SR pin 13 (Output enable pin) */
-const int latchClockPin = 10; /*Arduino pin 10 will be connected to SR pin 12 (Storage register clock input) - IDK why its called latchClockPin. I am just following naming conventions - */
+const int latchClockPin = 10; /*Arduino pin 10 will be connected to SR pin 12 (Storage register/Latch clock input)*/
 const int shiftClockPin = 9; /*Arduino pin 9 will be connected to SR pin 12 (Shift register clock input)*/
 
 const int SHIFT_REG_NUM = 4; /*The total number of shift registers being daisy-chained*/
 const int PIN_TOTAL = 8 * SHIFT_REG_NUM; /*Total number of output pins available*/
 const int RGB_LED_NUM = 12; /*The total number of RGB LEDs*/
 
-boolean pinState[PIN_TOTAL];
+boolean pinState[PIN_TOTAL];/*Bitmap/Bit array that stores the ON/OFF state for all output bits sent to the shift registers*/
 
-/*There are 24 total LED Anodes(12 Red, 12 Green) being driven by SR output pins.
- *For the sake of organization, each pin driving an LED anode has been given an "index".
- *The arrays below contain the indexes of SR output pins driving red and green anodes respectively. 
+/*There are 12 LEDs with 2 anodes each; 24 anodes total(12 Red, 12 Green), each is driven by SR output pins.
+ *The redPin and greenPin arrays map the 24 LED anodes to specific bit positions in the pinState array. 
 */
 
-int redPin[] = {1,3,5,7,9,11,13,15,17,19,21,23}; 
-int greenPin[] = {0,2,4,6,8,10,12,14,16,18,20,22};
+int redPin[] = {1,3,5,7,9,11,13,15,17,19,21,23}; /*Bit indices for red anodes of LEDs*/
+int greenPin[] = {0,2,4,6,8,10,12,14,16,18,20,22};/*Bit indices for green anodes of LEDs*/
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -58,6 +59,43 @@ void enableOutputs(boolean outputState){
 /*The pinStateWrite function is supposed to allow control the state of each SR pin linked to an LED Anode.*/
 void pinStateWrite(int index, int value){
   digitalWrite(latchClockPin,LOW);
-  
-  
+  pinState[index] = value;
+  for (int i = 0; i < SHIFT_REG_NUM; i++){
+    byte bitmap = 0;
+    int base = i*8;
+    for(int j = 0; j < 8; j++){
+      if(pinState[base+j]) bitSet(bitmap, j);
+    }
+    shiftOut(dataPin, clockPin, LSBFirst, bitmap);
+  }
+  digitalWrite(latchClockPin,HIGH);
 }
+
+/*--------------------------------SHIFT REGISTER OPERATION EXPLAINED--------------------------------*/
+/* The 74HC595 operates using two internal registers.
+ * 1. Shift Register - Temporarily holds data as it's being received.
+ * 2. Storage Register (Latch) - Holds the final data and controls the output pins (Q_A to Q_H)
+ *
+ * Data is transmitted into the shift register serially (bit-by-bit) through the serial input pin.
+ * At each rising edge (transition from LOW to HIGH) in the shift register clock signal, all bits
+ * in the shift register shift one position left (from LSB to MSB) and the current LSB is updated 
+ * with the current logic level at the serial input pin. This process continues until all 8 bits are
+ * loaded into the shift register. Once all 8 bits are loaded, a pulse is sent to the latch clock pin
+ * and all data is copied from the shift register to the storage register. 
+ * The outputs Q_A to Q_H are updated based on the values in the storage register.
+*/
+
+/*--------------------------------DAISY-CHAINING EXPLAINED--------------------------------*/
+/* A daisy chain is a wiring scheme where multiple devices are wired together in sequence or in a ring
+ * in a manner such that the output of one connects to the input of the next. 
+ * This project required control of many
+ * The Q_H' output pin of the 74HC595 allows the IC to be daisy-chained. The pin is serial output. 
+ * It outputs the last bit (Q_H) of the shift register. By wiring Q_H' from one chip to the serial input
+ * of the next multiple shift registers can be chained together. Each additional shift register adds an extra
+ * 8 outputs, all of them controlled using the same 4 Arduino pins - dataPin, latchClockPin, enablePin, 
+ * and shiftClockPin. 
+*/
+  
+  
+
+
